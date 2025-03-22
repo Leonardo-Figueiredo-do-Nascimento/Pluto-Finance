de6ceb08ad09837a1029a8c101be124d42207b2d
LEONARDO FIGUEIREDO DO NASCIMENTO <kriptonascimento@hotmail.com>
2025-03-22 15:07:42 -0300

    Criação de paginas de recuperação de senha

---
+++ lib/pages/PasswordCode/PasswordCodePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PasswordCodePage extends StatefulWidget {
+  const PasswordCodePage({super.key});
+
+  @override
+  State<PasswordCodePage> createState() => _PasswordCodePageState();
+}
+
+class _PasswordCodePageState extends State<PasswordCodePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
